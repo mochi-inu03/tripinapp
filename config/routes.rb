0943cb442fd4386538cca2dbaf9f1a0b4af3c3dd
mochi-inu03 <nissyuika@gmail.com>
2025-06-12 12:16:49 +0900

    投稿詳細表示機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'spots#index'
 
   devise_for :users
-  resources :spots, only: [:new, :create]
+  resources :spots, only: [:new, :create, :index, :show]
 end
